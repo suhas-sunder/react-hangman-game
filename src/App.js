dfdaa7ae3226003a1889c4205cf79f0b0a2a555d
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-03-25 00:15:05 -0400

    Fetch data
    Added states and API call to generate a random word for game.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+
 function App() {
+  const [finalAnswer, setFinalAnswer] = useState(null);
+  const [selectedLetter, setSelectedLetter] = useState([])
+
+  useEffect(() => {
+    fetch('https://random-word-api.vercel.app/api?words=1')
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      setFinalAnswer(data);
+    })
+  }, []);
+
   return (
     <div className="App">
+      <h1>HI!</h1>
     </div>
   );
 }
